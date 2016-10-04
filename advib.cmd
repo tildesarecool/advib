e8fd19e53370009149b4d2f2dc5728472414c8cf
tildesarecool <tildesare.cool@gmail.com>
2016-10-03 20:29:11 -0700

    Added a "debug" mode (which I haven't debugged). And am testing the skip problem still.

--- advib.cmd
+++ advib.cmd
@@ -1,4 +1,6 @@
-@echo off
+if [%4]==[debug] set debug=1
+
+if %debug%==1 ( @echo off )
 
 REM This version I did on the 11th
 REM And DOES work correctly in every way I can tell
@@ -6,12 +8,14 @@ REM Newer version
 REM Oct 3 2016
 REM New functionality added
 
+rem ==debug (set debug=1 && echo Value of debug is %debug%)
+rem  if [%4]==debug (set debug=1 && echo Value of debug is %debug%)) else ( goto :usagehelp )
 rem   this will output time, like 4:02:19
 for /f "usebackq tokens=7 delims= " %%i in (`NET TIME \\localhost`) do set currtime=%%i
-Rem this will output the last part, am or pm:
+rem this will output the last part, am or pm:
 for /f "usebackq tokens=8 delims= " %%i in (`NET TIME \\localhost`) do set amorpm=%%i
 
-pause
+if %debug%==1 ( echo debug is %debug% && pause )
 
 rem set gamelist=
 rem set CDDRIVE=
@@ -30,15 +34,14 @@ REM possible bonus feature: use a "title" statement to display the driver
 REM letter advib is using via the CDDRIVE variable
 
 if  [%1]==[] (goto :usagehelp) ELSE set gamelist=%1
-echo value is %gamelist%
+
+if %debug%==1 ( echo value is %gamelist% )
 rem pause
 
 if [%2]==[] (goto :usagehelp) ELSE SET CDDRIVE=%2
 if [%3]==[] (goto :usagehelp) ELSE SET CONSOLE=%3
 
-echo gamelist is %gamelist%
-echo drive is %CDDRIVE%
-echo console is %CONSOLE%
+if %debug%==1 ( echo gamelist is %gamelist% && echo drive is %CDDRIVE% && echo console is %CONSOLE% )
 
 rem pause
 if defined gamelist  goto :continueon ELSE goto :usagehelp
@@ -67,10 +70,7 @@ REM It doesn't seem to work with "discovery" mode
 REM It seems "read" mode will also work. I don't think there's a an advantage of one over the other
 ImgBurn.exe /mode verify /src %CDDRIVE%  /info "gameinfo\%GAMENAME%_%CONSOLE%.txt" /CLOSEINFO /WAITFORMEDIA
 
-echo right before info text read the values are:
-echo gamelist is %gamelist%
-echo drive is %CDDRIVE%
-echo console is %CONSOLE%
+if %debug%==1 ( echo right before info text read the values are: && echo gamelist is %gamelist% && echo drive is %CDDRIVE% && echo console is %CONSOLE% )
 rem pause
 if exist "gameinfo\%GAMENAME%_%CONSOLE%.txt" (for /f "usebackq tokens=3 delims= " %%z in (`type gameinfo\%GAMENAME%_%CONSOLE%.txt ^| find /i "current profile"`) do set DISCTYPE=%%z)  else (echo could not find text file)
 
@@ -84,15 +84,16 @@ REM and set the CUE extension variable for verifying
 REM if /i "%CONSOLE%" == "ps1" (set FILE-EXT=BIN && set VEREXT=CUE)
 
 if /i "%DISCTYPE%" == "cd-rom" (set FILE-EXT=BIN && set VEREXT=CUE)  else (SET FILE-EXT=ISO && set VEREXT=ISO)
-echo value of FILE-EXT is %FILE-EXT%
-echo value of VEREXT is %VEREXT%
+
+if %debug%==1 ( echo value of FILE-EXT is %FILE-EXT% && echo value of VEREXT is %VEREXT% )
 rem pause
 
 REM made it all the way to here and couldn't find the s:\playstation-rips directory, of course
 REM I ended up just using the regular % ~ d p 0 as the solution (that dp0 expands, even in REMs)
 
 ImgBurn.exe /mode read /src %CDDRIVE%  /dest %~dp0%CONSOLE%\%GAMENAME%_%CONSOLE%.%FILE-EXT% /start /CLOSESUCCESS /WAITFORMEDIA
-echo error level is %errorlevel%
+
+if %debug%==1 ( echo error level is %errorlevel% )
 REM pause
 
 
@@ -111,8 +112,7 @@ for /f "usebackq tokens=1 delims= " %%i in (`fciv %console%\%gamename%_%console%
 for /f "usebackq tokens=1 delims= " %%i in (`fciv %CONSOLE%\%GAMENAME%_%console%.%VEREXT% -md5  ^| find /v "/"`) do set md5hash=%%i
 
 
-
-echo sha1 hash is %verext%_%shahash% and md5 hash is %verext%_%md5hash%
+if %debug%==1 ( echo sha1 hash is %verext%_%shahash% and md5 hash is %verext%_%md5hash% )
 
 if not exist gameslog.csv (echo console,game_name,file-type,sha1_hash,date,time > gameslog.csv && echo %console%,%gamename%,%FILE-EXT%,%shahash%,%date%,%currtime%%amorpm% >> gameslog.csv) else (echo %console%,%gamename%,%FILE-EXT%,%shahash%,%date%,%currtime%%amorpm% >> gameslog.csv)
 
