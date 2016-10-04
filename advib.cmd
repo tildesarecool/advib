ff02ffce44f42d0486bd0838d72801ede647bce2
tildesarecool <tildesare.cool@gmail.com>
2016-10-03 20:46:36 -0700

    Slightly more working version of "debug mode". It seems to be working correctly.

--- advib.cmd
+++ advib.cmd
@@ -1,6 +1,9 @@
-if [%4]==[debug] set debug=1
+set debug=0
 
-if %debug%==1 ( @echo off )
+if [%4]==[debug] (set debug=1)
+
+IF %debug%==1 (echo on) else (echo off)
+rem  ELSE (@echo off)
 
 REM This version I did on the 11th
 REM And DOES work correctly in every way I can tell
